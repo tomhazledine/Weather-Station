b3844da7732ccc051cb7518cb4fbb37f0d4500f0
Tom Hazledine <tom@eatenbymonsters.com>
2016-03-23 19:46:31 +0000

    basic filesystem functionality

--- nodetest.js
+++ nodetest.js
@@ -1,19 +1,23 @@
 var PythonShell = require('python-shell');
+var fs = require('fs');
+
+// Create a log file
+fs.writeFileSync('logfile.csv','time,message\n');
+console.log('made new logfile.csv\n');
+
+// Run out python script
 var pyshell = new PythonShell('test.py');
-
-// var testMessage = {
-// 	'message': 'A test message'
-// }
-
-// sends a message to the Python script via stdin
-// pyshell.send('hello');
 
 pyshell.on('message', function (message) {
   // received a message sent from the Python script (a simple "print" statement)
   var newMessage = JSON.parse(message);
-  console.log(newMessage.first_name);
-
+  writeMessage(newMessage.middle_name);
 });
+
+function writeMessage(message){
+    fs.appendFile('logfile.csv',`${message.trim()}\n`);
+    console.log('logged message to file\n');
+}
 
 // end the input stream and allow the process to exit
 // pyshell.end(function (err) {
