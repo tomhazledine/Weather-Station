a82b03949fc0e7b370e50ded4a93b97dd1065ab2
Tom Hazledine <tom@tomhazledine.com>
2016-03-24 08:50:21 +0000

    created npm package

--- test.py
+++ test.py
@@ -1,26 +1,7 @@
 import json
-import sys
-
-# print("test.py running...")
-
-# json_string = '{"first_name": "Tom", "last_name":"Hazledine"}'
-
-# print json_string
 
 newstring = 'Tom'
 
-# for line in sys.stdin:
-	# print line
-	# parsedLine = json.loads(line)
-	# print parsedLine
-
-
-# Test JSON parsing:
-# json_string = '{"first_name": "Tom", "last_name":"Hazledine"}'
-# parsed_json = json.loads(json_string)
-# # print(parsed_json['first_name'])
-
-# Test JSON output:
 data = {
     'first_name': newstring,
     'last_name': 'Hazledine',
@@ -30,5 +11,3 @@ data = {
 data['middle_name'] = 'Frederick';
 
 print(json.dumps(data))
-
-# print('test.py finished.')
