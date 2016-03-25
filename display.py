f1f6fa7cd0f65ade1cdb35cf659c9b10fe9bcf03
Tom Hazledine <tom@eatenbymonsters.com>
2016-03-25 11:31:51 +0000

    display test

--- display.py
+++ display.py
@@ -13,6 +13,17 @@ i = [75, 0, 130]
 v = [159, 0, 255]
 e = [0, 0, 0]  # e stands for empty/black
 
+image00 = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e
+]
+
 image0 = [
 e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,
@@ -43,7 +54,7 @@ e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,
 e,e,w,e,e,e,e,e,
 e,w,e,e,e,e,e,e,
-w,e,e,e,e,e,e,e
+e,e,e,e,e,e,e,e
 ]
 
 image3 = [
@@ -53,19 +64,29 @@ e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,
 e,e,e,w,e,e,e,e,
 e,e,w,e,e,e,e,e,
-e,w,e,e,e,e,e,e,
-w,e,e,e,e,e,e,e
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e
 ]
 
 image4 = [
 e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,
-e,e,e,e,r,e,e,e,
+e,e,e,e,w,e,e,e,
 e,e,e,w,e,e,e,e,
-e,e,w,e,e,e,e,e,
-e,w,e,e,e,e,e,e,
-w,e,e,e,e,e,e,e
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e
+]
+image4_1 = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,r,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e
 ]
 
 image5 = [
@@ -90,9 +111,9 @@ e,w,e,e,e,e,e,e,
 w,e,e,e,e,e,e,e
 ]
 
-images = [image0,image1,image2,image3,image4,image5,image6]
+images = [image00,image0,image1,image2,image3,image4,image5,image6]
 
 while True:
     for image in images:
         sense.set_pixels(image)
-        time.sleep(1)
+        time.sleep(.2)
