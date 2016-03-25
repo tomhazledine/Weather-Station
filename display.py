00d80b034537d1ad33eca8addcae0bc0bc666e86
Tom Hazledine <tom@eatenbymonsters.com>
2016-03-25 11:26:59 +0000

    display test

--- display.py
+++ display.py
@@ -1,7 +1,9 @@
 from sense_hat import SenseHat
+import time
 
 sense = SenseHat()
 
+w = [255,255,255]
 r = [255, 0, 0]
 o = [255, 127, 0]
 y = [255, 255, 0]
@@ -11,15 +13,84 @@ i = [75, 0, 130]
 v = [159, 0, 255]
 e = [0, 0, 0]  # e stands for empty/black
 
-image = [
+images[0] = [
 e,e,e,e,e,e,e,e,
-e,e,e,r,r,e,e,e,
-e,r,r,o,o,r,r,e,
-r,o,o,y,y,o,o,r,
-o,y,y,g,g,y,y,o,
-y,g,g,b,b,g,g,y,
-b,b,b,i,i,b,b,b,
-b,i,i,v,v,i,i,b
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+w,e,e,e,e,e,e,e
 ]
 
-sense.set_pixels(image)
+images[1] = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,w,e,e,e,e,e,e,
+w,e,e,e,e,e,e,e
+]
+
+images[2] = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,w,e,e,e,e,e,
+e,w,e,e,e,e,e,e,
+w,e,e,e,e,e,e,e
+]
+
+images[3] = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,w,e,e,e,e,
+e,e,w,e,e,e,e,e,
+e,w,e,e,e,e,e,e,
+w,e,e,e,e,e,e,e
+]
+
+images[4] = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,r,e,e,e,
+e,e,e,w,e,e,e,e,
+e,e,w,e,e,e,e,e,
+e,w,e,e,e,e,e,e,
+w,e,e,e,e,e,e,e
+]
+
+images[5] = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,r,e,e,e,
+e,e,e,r,g,r,e,e,
+e,e,e,w,r,e,e,e,
+e,e,w,e,e,e,e,e,
+e,w,e,e,e,e,e,e,
+w,e,e,e,e,e,e,e
+]
+
+images[5] = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,r,e,e,e,
+e,e,e,r,g,r,e,e,
+e,e,r,g,b,g,r,e,
+e,e,e,r,g,r,e,e,
+e,e,w,e,r,e,e,e,
+e,w,e,e,e,e,e,e,
+w,e,e,e,e,e,e,e
+]
+
+white True:
+    for image in images:
+        sense.set_pixels(image)
+        time.sleep(1)
